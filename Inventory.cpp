f587d133efc842077662259c8a6409ec19e56274
Ghimpusan Alexandru <ghimpusan_alexandru@yahoo.com>
2022-03-10 10:51:55 -0800

    Implemented the Delete Product function

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,3 +1,6 @@
+#include <iterator>
+#include <list>
+#include <iostream>
 #include "Inventory.h"
 
 Inventory::Inventory()
@@ -10,9 +13,11 @@ Inventory::~Inventory()
 
 void Inventory::displayInventory()
 {
+	std::cout << "\n\n******************************************************************";
 	for (Product* p : _inv) {
 		p->displayProduct();
 	}
+	std::cout << "\n******************************************************************";
 }
 
 void Inventory::addProduct(Product* newProd)
@@ -22,7 +27,17 @@ void Inventory::addProduct(Product* newProd)
 
 void Inventory::deleteProduct(std::string nameProd)
 {
-	//To Do
+
+	std::list<Product*>::iterator it;
+
+	for (it = this->_inv.begin(); it != this->_inv.end(); it++) {
+		if ((*it)->getName() == nameProd) {
+			std::cout << "\nProduct " << nameProd << " was erased";
+			this->_inv.erase(it);
+			break;
+		}
+	}
+
 }
 
 
