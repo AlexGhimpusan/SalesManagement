ed657448d34023f48fc65499705600eab831e974
Ghimpusan Alexandru <ghimpusan_alexandru@yahoo.com>
2022-03-10 10:52:10 -0800

    Started using the Inventory

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "Inventory.h"
+#include "Product.h"
 
 int main()
 {
-    std::cout << "Hello my inventory!\n";
+    Inventory* myInv = new Inventory();
+
+    Product* p = new Product("Lapte", 5, 2);
+    myInv->addProduct(p);
+
+    p = new Product("Carne", 10, 3);
+    myInv->addProduct(p);
+
+    myInv->displayInventory();
+
+    myInv->deleteProduct("Cartofi");
+    myInv->displayInventory();
+
+    myInv->deleteProduct("Carne");
+    myInv->displayInventory();
+
 }
 
